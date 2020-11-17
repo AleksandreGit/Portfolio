bfdefc63330ad0e23ff73f2471a394d470045316
Alexandre <achapin@insa-rennes.fr>
2020-11-17 17:58:00 +0100

    Create the base of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './profile/navbar/navbar.component';
+import { IntroComponent } from './profile/intro/intro.component';
+import { PresentationComponent } from './profile/presentation/presentation.component';
+import { FooterComponent } from './profile/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    PresentationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
