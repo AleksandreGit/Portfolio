f3047ec17ff7b9ec4cf301827aa1ab1b690e55c6
Alexandre <achapin@insa-rennes.fr>
2020-11-23 21:45:05 +0100

    Add skills animation

--- src/app/profile/skills/skills.component.ts
+++ src/app/profile/skills/skills.component.ts
@@ -12,11 +12,38 @@ export class SkillsComponent implements OnInit {
   // Progress bar
   color: ThemePalette = 'primary';
   mode: ProgressBarMode = 'determinate';
-  value = 50;
+  public values: number[];
 
-  constructor() { }
+  private observer: IntersectionObserver;
+
+  constructor() {
+    // Values of the progress bars
+    this.values = [85, 85, 75, 70, 60, 55, 45, 35];
+    // Definition of the observer
+    this.observer = new IntersectionObserver(observables => {
+      observables.forEach(observ => {
+        if (observ.intersectionRatio > 0) {
+          observ.target.classList.remove('not-visible');
+        }
+        else{
+          observ.target.classList.add('not-visible');
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
+    const items = document.querySelectorAll('.bar-skills');
+    items.forEach(item => {
+      item.classList.add('not-visible');
+      this.observer.observe(item);
+    });
+
+    const names = document.querySelectorAll('.name-skill');
+    names.forEach(item => {
+      item.classList.add('not-visible');
+      this.observer.observe(item);
+    });
   }
 
 }
