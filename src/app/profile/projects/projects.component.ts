ee22faae5bd0ef82affdd5c22f8f39ee0509e67f
Alexandre <achapin@insa-rennes.fr>
2020-11-24 11:46:13 +0100

    Add projects animation

--- src/app/profile/projects/projects.component.ts
+++ src/app/profile/projects/projects.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  private observer: IntersectionObserver;
+
+  constructor() {
+    this.observer = new IntersectionObserver(observables => {
+      observables.forEach(observ => {
+        if (observ.intersectionRatio > 0) {
+          observ.target.classList.remove('not-visible');
+        }
+        else{
+          observ.target.classList.add('not-visible');
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
+    const items = document.querySelectorAll('.proj-body');
+    items.forEach(item => {
+      item.classList.add('not-visible');
+      this.observer.observe(item);
+    });
   }
 
 }
