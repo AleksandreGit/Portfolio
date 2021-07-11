e4970c105708c5a2d90253506c37127128184746
Wheely <alexchapin@hotmail.fr>
2021-07-11 10:10:36 +0200

    Add last projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import {ThurstonComponent} from './profile/projects/thurston/thurston.component'
 import {VaisseauComponent} from './profile/projects/vaisseau/vaisseau.component';
 import {MasquesComponent} from './profile/projects/masques/masques.component';
 import {XrayComponent} from './profile/projects/xray/xray.component';
+import {MoteurjeuComponent} from './profile/projects/moteurjeu/moteurjeu.component';
+import {JeuvideoComponent} from './profile/projects/jeuvideo/jeuvideo.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
@@ -20,6 +22,8 @@ const routes: Routes = [
   { path: 'vaisseau', component: VaisseauComponent },
   { path: 'masque', component: MasquesComponent },
   { path: 'xray', component: XrayComponent },
+  { path: 'moteur', component: MoteurjeuComponent},
+  { path: 'jeu_video', component: JeuvideoComponent},
 ];
 
 @NgModule({
